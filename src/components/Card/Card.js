00d1813466b3e0b7e58a7a73f51795fe7e4ceb27
Mateusz Myszke <myszke93@gmail.com>
2023-03-09 09:10:06 +0100

    Add action ADD_CARD and ADD_COLUMN in reducer

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,6 +3,6 @@ import styles from './Card.module.scss';
 const Card = props =>{
   return(
     <li className={styles.card}>{props.title}</li>
-  );
+  )
 };
 export default Card;
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -15,7 +15,7 @@ const Column = props =>{
         <ul className={styles.cards}>
           {cards.map(card => <Card key={card.id} {...card} />)}
         </ul>
-        <CardForm columnId={props.id} action={props.action}/>
+        <CardForm columnId={props.id} />
       </article>
   );
 };
