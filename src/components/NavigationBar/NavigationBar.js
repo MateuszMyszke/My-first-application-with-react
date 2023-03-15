a75c8bf7b094d56dab2c2e6e48d4e0012a0d686d
Mateusz Myszke <myszke93@gmail.com>
2023-03-15 11:30:50 +0100

    Add style isActive in NavigationBar

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,24 +1,19 @@
 import styles from './NavigationBar.module.scss';
 import Container  from '../Container/Container';
+import { NavLink, Link } from 'react-router-dom';
 
 const NavigationBar = () => {
   return(
     <nav className={styles.navBar}>
       <Container>
         <div className={styles.navWrapper}>
-          <a href='/'>
+          <Link to='/'>
             <span className='fa fa-tasks'></span>
-          </a>
+          </Link>
           <ul>
-            <li>
-              <a href='/'>Home</a>
-            </li>
-            <li>
-              <a href='/favorite'>Favorite</a>
-            </li>
-            <li>
-              <a href='/about'>About</a>
-            </li>
+            <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to='/'>Home</NavLink></li>
+            <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to='/favorite'>Favorite</NavLink></li>
+            <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to='/about'>About</NavLink></li>
           </ul>
         </div>
       </Container>
